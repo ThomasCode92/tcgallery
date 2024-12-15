764e0bf4cb29dd6428dc22d9373f4b29308cc7e7
Thomas <thomascode92@hotmail.com>
2024-12-15 14:37:07 +0100

    Set Up a Database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 // Images are hosted on uploadthing.com
 const mockUrls = [
   "https://utfs.io/f/reMSMTRIWajNKW1mEfcmV0Mo2UQRkbnHlgptwvd3ycIESFKu",
@@ -6,12 +8,23 @@ const mockUrls = [
 
 const mockImages = mockUrls.map((url, idx) => ({ idx: idx + 1, url }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
+
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map(({ idx, url }) => (
-          <img key={idx} src={url} alt={`Image ${idx}`} className="w-48" />
+        {posts.map(post => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, idx) => (
+          <img
+            key={idx}
+            src={image.url}
+            alt={`Image ${idx}`}
+            className="w-48"
+          />
         ))}
       </div>
     </main>
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,18 +1,15 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/neon-http";
 
-import { env } from "~/env";
 import * as schema from "./schema";
 
-/**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
- */
-const globalForDb = globalThis as unknown as {
-  conn: postgres.Sql | undefined;
-};
+config({ path: ".env" }); // or .env.local
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
-if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined in the environment variables");
+}
 
-export const db = drizzle(conn, { schema });
+const sql = neon(process.env.DATABASE_URL);
+
+export const db = drizzle(sql, { schema });
