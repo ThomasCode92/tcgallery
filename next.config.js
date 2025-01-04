6ea635a149008f0a3e479f3fed1090fcd11dce3e
Thomas <thomascode92@hotmail.com>
2025-01-04 20:20:11 +0100

    Basic PostHog Analytics

--- next.config.js
+++ next.config.js
@@ -10,6 +10,23 @@ const coreConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
   images: { remotePatterns: [{ hostname: "utfs.io" }] },
+  skipTrailingSlashRedirect: true,
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+      {
+        source: "/ingest/decide",
+        destination: "https://us.i.posthog.com/decide",
+      },
+    ];
+  },
 };
 
 const config = withSentryConfig(coreConfig, {
